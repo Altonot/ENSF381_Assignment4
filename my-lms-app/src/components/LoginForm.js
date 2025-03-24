d0463f8cb3bca232fe70520ce878f2e98e9f26d2
Altinb <altin_15.2004@hotmail.com>
2025-03-24 00:27:58 -0600

    cont. login

--- my-lms-app/src/components/LoginForm.js
+++ my-lms-app/src/components/LoginForm.js
@@ -3,7 +3,10 @@ import './LoginForm.css';
 function LoginForm() {
     return (
         <div>
-            
+            <input type='text' id='user' name='username' placeholder='Input Username...' required />
+            <input type='password' id='pass' name='password' placeholder='Input Password...' required />
+            <button id='login'>Login</button>
+            <a>Forgot Password?</a>
         </div>
     );
 }
